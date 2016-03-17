110bac2058b3c28fece830955e55429357afadc0
Alexander Rasputin <alexander.rasputin@uni-ulm.de>
2016-03-17 15:31:43 +0100

    new rofi style

---
+++ config/i3/scripts/rofi.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rofi -modi "run,drun" -show drun -lines 4 -eh 2 -width 1366 -location 0 -padding 305 -opacity "70" -monitor 0
--- config/i3/scripts/screenshot.sh
+++ config/i3/scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #! /bin/zsh
 scrot '%Y-%m-%d_$wx$h_scrot.png' -e 'mv $f ~/Pictures/Screenshots'
 sleep 0.1
-Thunar ~/Pictures/Screenshots
+Thunar ~/Media/Pictures/Screenshots
