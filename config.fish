5b209ef62c35880df65e107f11354a28b40894f4
Alexander Rasputin <rasputin.alex@hotmail.de>
2022-04-09 13:16:46 +0200

    feat: reworking setup

--- config.fish
+++ config.fish
@@ -1,11 +1,13 @@
 # Remove fish default greeting
 set --erease fish_greeting
 
-# Enviroment Variables
-set -x NPM_TOKEN 
+# so our brew install override the commands from the system
+set -x PATH /usr/local/sbin $PATH
+
+# Environment Variables
+set -x NPM_TOKEN
 set -x ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 set -x PATH /Applications/Flutter/bin
-set -x NVM_DIR 
-
+set -x NVM_DIR
 
 starship init fish | source
